6d4fd7d997dfe8605141f1c5207ecd50f96d0c4e
lgz <lgz940228@163.com>
2018-10-15 18:19:11 +0800

    feat:添加dispacherServlet urlMapping

---
+++ src/main/java/com/lgz/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package com.lgz.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@SpringBootConfiguration
+public class MvcConfig {
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean servletServletRegistrationBean = new ServletRegistrationBean(dispatcherServlet);
+        servletServletRegistrationBean.addUrlMappings("*.do");
+        servletServletRegistrationBean.addUrlMappings("*.html");
+        servletServletRegistrationBean.addUrlMappings("*.css");
+        servletServletRegistrationBean.addUrlMappings("*.js");
+        servletServletRegistrationBean.addUrlMappings("*.png");
+        servletServletRegistrationBean.addUrlMappings("*.gif");
+        servletServletRegistrationBean.addUrlMappings("*.ico");
+        servletServletRegistrationBean.addUrlMappings("*.jpeg");
+        servletServletRegistrationBean.addUrlMappings("*.jpg");
+        return servletServletRegistrationBean;
+    }
+}
